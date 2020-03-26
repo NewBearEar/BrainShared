dea4277fb07b1333064f7e9934f10a5cde388f9e
Naraku <Naraku-Mo@users.noreply.github.com>
2020-03-26 15:03:13 +0800

    修改了主题颜色

---
+++ app/src/main/java/com/example/a123/sharedbrain/DataModel/ActivityModel.java
@@ -0,0 +1,30 @@
+package com.example.a123.sharedbrain.DataModel;
+
+public class ActivityModel {
+
+    private String title;
+    private int id;
+
+    public ActivityModel(int id,String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
